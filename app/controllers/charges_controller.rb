225e05231eb3cdd4d4a0261f9110f2a5e3cb4314
luis <hilario.huaman.6@gmail.com>
2022-06-12 11:41:26 -0500

    add config figaro

---
+++ app/controllers/charges_controller.rb
@@ -0,0 +1,32 @@
+class ChargesController < ApplicationController
+  def index
+    @charges = Charge.all
+  end
+
+  def new
+    @charge = Charge.new
+  end
+
+  def create
+    @charge = Charge.new(charge_params)
+    @charge.user = current_user
+    @charge.shopping_cart = current_user.shopping_cart
+    if @charge.save
+      @signature = signature(@charge)
+      render :payu
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def charge_params
+    params.require(:charge).permit(:amount)
+  end
+
+  def signature(charge)
+    msg = "#{ENV["PAYU_API_KEY"]}~#{ENV["PAYU_MERCHANT_ID"]}~#{charge.uid}~#{charge.amount}~COP"
+    Digest::MD5.hexdigest(msg)
+  end
+end

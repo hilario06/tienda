9d0c0d5854f658c98b05b709eb12bb515cfbae9f
luis <hilario.huaman.6@gmail.com>
2022-07-06 16:19:03 -0500

    add notice al añadir productos de deseos

--- app/controllers/product_likes_controller.rb
+++ app/controllers/product_likes_controller.rb
@@ -6,8 +6,19 @@ class ProductLikesController < ApplicationController
 
   def create
     product_likes = ProductLike.find_by(product_id: params[:product], user_id: current_user)
+    message_notice = 'Ya fue añadido a la lista de deseos'
     unless product_likes
       ProductLike.create(product_id: params[:product], user: current_user)
+      message_notice = 'Añadido a la lista de deseos'
+    end
+
+    case params[:a_like_from].to_i
+    when 1
+      redirect_to products_path, notice: message_notice
+    when 2
+      redirect_to product_path(params[:product]), notice: message_notice
+    when 3
+      redirect_to shopping_carts_path, notice: message_notice
     end
   end
 
@@ -15,6 +26,6 @@ class ProductLikesController < ApplicationController
     product_id = params[:id]
     @product_likes = ProductLike.find_by(product_id: product_id, user_id: current_user)
     @product_likes.destroy
-    redirect_to product_likes_index_url
+    redirect_to product_likes_url
   end
 end

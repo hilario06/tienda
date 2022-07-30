ceae753bf111d02c17f02e0a4b1638a9e01a836b
luis <hilario.huaman.6@gmail.com>
2022-07-30 17:30:25 -0500

    add routes not redes sociales

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # devise_for :users
-  devise_for :users, controllers: {omniauth_callbacks: 'users/omniauth_callbacks'}
+  # devise_for :users, controllers: {omniauth_callbacks: 'users/omniauth_callbacks'}
+  devise_for :users
   root to: "pages#home"
   resources :categories do
     # categories/:category_id/restore

870daf40b7d37e20a2a65a6fd9807123aa9e7fad
luis <hilario.huaman.6@gmail.com>
2022-08-04 16:03:24 -0500

    agregando metodo configure_permitted_parameters para cepte el campo de admin del modelo User

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,21 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
-
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   include Pundit::Authorization
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
   # after_action :verify_authorized, except: :index
   # after_action :verify_policy_scoped, only: :index
+
+  protected
+
+  def configure_permitted_parameters
+    # For additional fields in app/views/devise/registrations/new.html.erb
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
+
+    # For additional in app/views/devise/registrations/edit.html.erb
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name])
+  end
 
   private
 
